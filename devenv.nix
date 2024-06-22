23e724a91694720af51734dc62e4f1551f22ec42
Ente <ducksource@duckpond.ch>
2024-06-22 04:53:33 +0200

    devenv help

--- devenv.nix
+++ devenv.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, config, lib, ... }:
 let
   CACHIX_CACHE = "duckpond";
 in
@@ -40,294 +40,362 @@ in
     cachix
   ]);
 
-  scripts.devenv_ready.exec = ''
-    echo "[+] Devenv Ready!"
-  '';
+  scripts.devenv_ready = {
+    description = "Tests if the devenv is ready";
+    exec = ''
+      echo "[+] Devenv Ready!"
+    '';
+  };
 
-  scripts.erase_flash.exec = ''
-    set -exuo pipefail
+  scripts.erase_flash = {
+    description = "Set esp32 flash to all 0x0";
+    exec = ''
+      set -exuo pipefail
 
-    tty="''${1:-/dev/ttyUSB0}"
-    esptool.py \
-      --port "''${tty}" \
-      erase_flash
-  '';
-
-  scripts.getchip.exec = ''
-    set -euo pipefail
-
-    tty="''${1:-/dev/ttyUSB0}"
-    esptool.py \
-      --port "''${tty}" \
-      read_mac \
-    | sed -nre 's/Chip is ([^ ]+).*/\1/p' \
-    | tr '[:upper:]' '[:lower:]'
-  '';
-
-  scripts.flash.exec = ''
-    set -exuo pipefail
-
-    tty="''${1:-/dev/ttyUSB0}"
-    (
-      cd "''${DEVENV_ROOT}"
-      set -x
-
-      chip_id="''$(getchip "''${tty}")"
-
-      declare -A chip_name=( \
-        ["esp32-c3"]="esp32-c3" \
-        ["esp32-d0wd"]="esp32" \
-        ["esp32-d0wd-v3"]="esp32" \
-      )
-
-      declare -A chip_fw=( \
-        ["esp32-c3"]="${./firmware/ESP32_GENERIC_C3-20240602-v1.23.0.bin}" \
-        ["esp32-d0wd"]="${./firmware/ESP32_GENERIC-20240602-v1.23.0.bin}" \
-        ["esp32-d0wd-v3"]="${./firmware/ESP32_GENERIC-20240602-v1.23.0.bin}" \
-      )
-
-      declare -A chip_addr=( \
-        ["esp32-c3"]="0x0" \
-        ["esp32-d0wd"]="0x1000" \
-        ["esp32-d0wd-v3"]="0x1000" \
-      )
-
+      tty="''${1:-/dev/ttyUSB0}"
       esptool.py \
-        --chip "''${chip_name["''$chip_id"]}" \
         --port "''${tty}" \
-        --baud 460800 \
-        write_flash \
+        erase_flash
+    '';
+  };
+
+  scripts.getchip = {
+    description = "Get the chip info for the connected esp32";
+    exec = ''
+      set -euo pipefail
+
+      tty="''${1:-/dev/ttyUSB0}"
+      esptool.py \
+        --port "''${tty}" \
+        read_mac \
+      | sed -nre 's/Chip is ([^ ]+).*/\1/p' \
+      | tr '[:upper:]' '[:lower:]'
+    '';
+  };
+
+  scripts.flash = {
+    description = "Flash your esp32 with clean Micropython firmware";
+    exec = ''
+      set -exuo pipefail
+
+      tty="''${1:-/dev/ttyUSB0}"
+      (
+        cd "''${DEVENV_ROOT}"
+        set -x
+
+        chip_id="''$(getchip "''${tty}")"
+
+        declare -A chip_name=( \
+          ["esp32-c3"]="esp32-c3" \
+          ["esp32-d0wd"]="esp32" \
+          ["esp32-d0wd-v3"]="esp32" \
+        )
+
+        declare -A chip_fw=( \
+          ["esp32-c3"]="${./firmware/ESP32_GENERIC_C3-20240602-v1.23.0.bin}" \
+          ["esp32-d0wd"]="${./firmware/ESP32_GENERIC-20240602-v1.23.0.bin}" \
+          ["esp32-d0wd-v3"]="${./firmware/ESP32_GENERIC-20240602-v1.23.0.bin}" \
+        )
+
+        declare -A chip_addr=( \
+          ["esp32-c3"]="0x0" \
+          ["esp32-d0wd"]="0x1000" \
+          ["esp32-d0wd-v3"]="0x1000" \
+        )
+
+        esptool.py \
+          --chip "''${chip_name["''$chip_id"]}" \
+          --port "''${tty}" \
+          --baud 460800 \
+          write_flash \
+            --compress \
+            "''${chip_addr["''$chip_id"]}" \
+            "''${chip_fw["''$chip_id"]}"
+      )
+    '';
+  };
+
+  scripts.repl = {
+    description = "Start an interactive read eval print loop from the connected esp32";
+    exec = ''
+      set -exuo pipefail
+
+      tty="''${1:-/dev/ttyUSB0}"
+      picocom \
+        -b115200 \
+        "''${tty}"
+    '';
+  };
+
+  scripts.repl_peek = {
+    description = "Show serial logs from the connected esp32";
+    exec = ''
+      set -exuo pipefail
+
+      tty="''${1:-/dev/ttyUSB0}"
+      sleep infinity | repl "''${tty}"
+    '';
+  };
+
+  scripts.fs = {
+    description = "Browse the file system from the connected esp32";
+    exec = ''
+      set -exuo pipefail
+
+      tty="''${1:-/dev/ttyUSB0}"
+      tty_name="''$(basename "''${tty}")"
+      mpfshell \
+        "''${tty_name}"
+    '';
+  };
+
+  scripts.alltty = {
+    description = "Run a command on all connected esp32's";
+    exec = ''
+      set -exuo pipefail
+
+      parallel \
+        --will-cite \
+        --tagstring "[{/.}]" \
+        --line-buffer \
+        --halt now,fail=1 \
+        "''${@} {}" ::: /dev/ttyUSB*
+    '';
+  };
+
+  scripts.upload = {
+    description = "Upload the content of your src/ directory to the connected esp32";
+    exec = ''
+      set -exuo pipefail
+
+      tty="''${1:-/dev/ttyUSB0}"
+      (
+        cd "''${DEVENV_ROOT}"
+        rshell \
+          --port "''${tty}" \
+          --file ./flash.cmds
+      )
+    '';
+  };
+
+  scripts.upload_lab = {
+    description = "Upload a lab in labs/ to the connected esp32";
+    exec = ''
+      set -euo pipefail
+
+      lab="''${1?Missing lab as first parameter}"
+      tty="''${2:-/dev/ttyUSB0}"
+      (
+        cd "''${DEVENV_ROOT}"
+        if [[ ! -e "labs/''${lab}" ]]; then
+          1>&2 echo "[!] Lab: ''${lab} does not exist"
+          exit 1
+        fi
+        echo "''${lab}" > labs/labselector.txt
+        rshell \
+          --port "''${tty}" \
+          --file ./flash_lab.cmds
+      )
+    '';
+  };
+
+  scripts.mkfirmware = {
+    description = "Build the ota firmware from the content of src/";
+    exec = ''
+      set -exuo pipefail
+
+      (
+        cd "''${DEVENV_ROOT}/src"
+        fw_tmp="$(mktemp -d)"
+
+        atexit(){
+          rm -rf "''${fw_tmp}"
+        }
+        trap atexit EXIT
+
+        PYTHONDONTWRITEBYTECODE=1 \
+          python fw_meta.py
+
+        rsync \
+          --archive \
+          --verbose \
+          --exclude="ota_firmware.py" \
+          *.py *.json www \
+          "''${fw_tmp}"
+
+        freezefs \
+          --target=/ \
+          --on-import=extract \
+          --overwrite=always \
           --compress \
-          "''${chip_addr["''$chip_id"]}" \
-          "''${chip_fw["''$chip_id"]}"
-    )
-  '';
+          "''${fw_tmp}" \
+          "ota_firmware.py"
+      )
+    '';
+  };
 
-  scripts.repl.exec = ''
-    set -exuo pipefail
+  scripts.clean_setup = {
+    description = "Do all steps required to get the firmware on the esp32";
+    exec = ''
+      set -exuo pipefail
 
-    tty="''${1:-/dev/ttyUSB0}"
-    picocom \
-      -b115200 \
-      "''${tty}"
-  '';
+      tty="''${1:-/dev/ttyUSB0}"
 
-  scripts.repl_peek.exec = ''
-    set -exuo pipefail
+      erase_flash "''${tty}"
+      flash "''${tty}"
+      mkfirmware "''${tty}"
+      upload "''${tty}"
+    '';
+  };
 
-    tty="''${1:-/dev/ttyUSB0}"
-    sleep infinity | repl "''${tty}"
-  '';
+  scripts.portmap = {
+    description = "Scan ports on the firmware";
+    exec = ''
+      set -exuo pipefail
 
-  scripts.fs.exec = ''
-    set -exuo pipefail
+      (
+        cd "''${DEVENV_ROOT}"
+        nmap 192.168.4.1
+      )
+    '';
+  };
 
-    tty="''${1:-/dev/ttyUSB0}"
-    tty_name="''$(basename "''${tty}")"
-    mpfshell \
-      "''${tty_name}"
-  '';
+  scripts.bforce = {
+    description = "Bruteforce passwords for the firmware's webui";
+    exec = ''
+      set -exuo pipefail
 
-  scripts.alltty.exec = ''
-    set -exuo pipefail
+      (
+        cd "''${DEVENV_ROOT}"
+        set -x
+        hydra \
+          -I \
+          -t 1 \
+          -l admin \
+          -P "${./10000_common_passwords}" \
+          http-get://192.168.4.1
+      )
+    '';
+  };
 
-    parallel \
-      --will-cite \
-      --tagstring "[{/.}]" \
-      --line-buffer \
-      --halt now,fail=1 \
-      "''${@} {}" ::: /dev/ttyUSB*
-  '';
+  scripts.cachix_push = {
+    description = "Push nix artifacts to cachix";
+    exec = ''
+      set -exuo pipefail
 
-  scripts.upload.exec = ''
-    set -exuo pipefail
+      (
+        cd "''${DEVENV_ROOT}"
+        cachix push "${CACHIX_CACHE}" "''${DEVENV_PROFILE}"
+      )
+    '';
+  };
 
-    tty="''${1:-/dev/ttyUSB0}"
-    (
-      cd "''${DEVENV_ROOT}"
-      rshell \
-        --port "''${tty}" \
-        --file ./flash.cmds
-    )
-  '';
-
-  scripts.upload_lab.exec = ''
-    set -euo pipefail
-
-    lab="''${1?Missing lab as first parameter}"
-    tty="''${2:-/dev/ttyUSB0}"
-    (
-      cd "''${DEVENV_ROOT}"
-      if [[ ! -e "labs/''${lab}" ]]; then
-        1>&2 echo "[!] Lab: ''${lab} does not exist"
-        exit 1
-      fi
-      echo "''${lab}" > labs/labselector.txt
-      rshell \
-        --port "''${tty}" \
-        --file ./flash_lab.cmds
-    )
-  '';
-
-  scripts.mkfirmware.exec = ''
-    set -exuo pipefail
-
-    (
-      cd "''${DEVENV_ROOT}/src"
-      fw_tmp="$(mktemp -d)"
-
-      atexit(){
-        rm -rf "''${fw_tmp}"
-      }
-      trap atexit EXIT
-
-      PYTHONDONTWRITEBYTECODE=1 \
-        python fw_meta.py
-
-      rsync \
-        --archive \
-        --verbose \
-        --exclude="ota_firmware.py" \
-        *.py *.json www \
-        "''${fw_tmp}"
-
-      freezefs \
-        --target=/ \
-        --on-import=extract \
-        --overwrite=always \
-        --compress \
-        "''${fw_tmp}" \
-        "ota_firmware.py"
-    )
-  '';
-
-  scripts.clean_setup.exec = ''
-    set -exuo pipefail
-
-    tty="''${1:-/dev/ttyUSB0}"
-
-    erase_flash "''${tty}"
-    flash "''${tty}"
-    mkfirmware "''${tty}"
-    upload "''${tty}"
-  '';
-
-  scripts.doalll.exec = ''
-    set -exuo pipefail
-
-    tty="''${1:-/dev/ttyUSB0}"
-
-    clean_setup "''${tty}"
-    repl "''${tty}"
-  '';
-
-  scripts.portmap.exec = ''
-    set -exuo pipefail
-
-    (
-      cd "''${DEVENV_ROOT}"
-      set -x
-      nmap 192.168.4.1
-    )
-  '';
-
-  scripts.bforce.exec = ''
-    set -exuo pipefail
-
-    (
-      cd "''${DEVENV_ROOT}"
-      set -x
-      hydra \
-        -I \
-        -t 1 \
-        -l admin \
-        -P "${./10000_common_passwords}" \
-        http-get://192.168.4.1
-    )
-  '';
-
-  scripts.cachix_push.exec = ''
-    set -exuo pipefail
-
-    (
-      cd "''${DEVENV_ROOT}"
-      cachix push "${CACHIX_CACHE}" "''${DEVENV_PROFILE}"
-    )
-  '';
-
-  scripts.uninstall_nix.exec = ''
-    set -exuo pipefail
-
-    (
-      cd "''${DEVENV_ROOT}"
+  scripts.uninstall_nix = {
+    description = "Print instructions on how to uninstall nix";
+    exec = ''
       echo "[*] See: https://nix.dev/manual/nix/2.22/installation/uninstall#multi-user"
-    )
+    '';
+  };
+
+  scripts.freezefs = {
+    description = "https://github.com/bixb922/freezefs";
+    exec = ''
+      python -m freezefs "''${@}"
+    '';
+  };
+
+  scripts.dns_amp = {
+    description = "Run the dns amplification attack";
+    exec = ''
+      set -exuo pipefail
+
+      (
+        cd "''${DEVENV_ROOT}"
+        sudo python dns_amp_solved.py "''${@}"
+      )
+    '';
+  };
+
+  scripts.monitor_network = {
+    description = "Show network statistics";
+    exec = ''
+      set -exuo pipefail
+
+      (
+        cd "''${DEVENV_ROOT}"
+        sudo iftop "''${@}"
+      )
+    '';
+  };
+
+  scripts.zlib_uncompress = {
+    description = "Uncompress zlib data from stdin";
+    exec = ''
+      printf "\x1f\x8b\x08\x00\x00\x00\x00\x00" \
+      | cat - "''${1}" \
+      | gzip -dc
+    '';
+  };
+
+  scripts.reset_update_devenv = {
+    description = "Reset and update the devenv";
+    exec = ''
+      echo "============= WARNING =============="
+      echo "This will reset and update your devenv"
+      echo "You will loose all your changes"
+      echo "Hit ENTER to continue"
+      echo "[ENTER]"
+      read
+
+      set -exuo pipefail
+      (
+        cd "''${DEVENV_ROOT}"
+        git reset --hard origin/master
+        git clean -f -f -d -x
+        git pull
+      )
+    '';
+  };
+
+  scripts.mount_firmware = {
+    description = "Mount the secret firmware directory";
+    exec = ''
+      set -exuo pipefail
+      (
+        cd "''${DEVENV_ROOT}"
+        gocryptfs -nonempty src_encrypted/ src/
+      )
+    '';
+  };
+
+  scripts.unmount_firmware = {
+    description = "Unmount the secret firmware directory";
+    exec = ''
+      set -exuo pipefail
+      (
+        cd "''${DEVENV_ROOT}"
+        umount src/
+      )
+    '';
+  };
+
+  scripts.devenv_help = {
+    description = "Print this help";
+    exec = ''
+      echo
+      echo Helper scripts you can run to make your development richer:
+      echo
+      ${pkgs.gnused}/bin/sed -e 's| |••|g' -e 's|=| |' <<EOF | ${pkgs.util-linuxMinimal}/bin/column -t | ${pkgs.gnused}/bin/sed -e 's|^|- |' -e 's|••| |g'
+      ${lib.generators.toKeyValue {} (lib.mapAttrs (name: value: value.description) config.scripts)}
+      EOF
+      echo
+    '';
+  };
+
+
+  enterShell = ''
+    devenv_help
   '';
-
-  scripts.freezefs.exec = ''
-    python -m freezefs "''${@}"
-  '';
-
-  scripts.dns_amp.exec = ''
-    set -exuo pipefail
-
-    (
-      cd "''${DEVENV_ROOT}"
-      sudo python dns_amp_solved.py "''${@}"
-    )
-  '';
-
-  scripts.monitor_network.exec = ''
-    set -exuo pipefail
-
-    (
-      cd "''${DEVENV_ROOT}"
-      sudo iftop "''${@}"
-    )
-  '';
-
-  scripts.zlib_uncompress.exec = ''
-    printf "\x1f\x8b\x08\x00\x00\x00\x00\x00" \
-    | cat - "''${1}" \
-    | gzip -dc
-  '';
-
-  scripts.reset_update_devenv.exec = ''
-    echo "============= WARNING =============="
-    echo "This will reset and update your devenv"
-    echo "You will loose all your changes"
-    echo "Hit ENTER to continue"
-    echo "[ENTER]"
-    read
-
-    set -exuo pipefail
-    (
-      cd "''${DEVENV_ROOT}"
-      git reset --hard origin/master
-      git clean -f -f -d -x
-      git pull
-    )
-  '';
-
-  scripts.mount_firmware.exec = ''
-    set -exuo pipefail
-    (
-      cd "''${DEVENV_ROOT}"
-      gocryptfs -nonempty src_encrypted/ src/
-    )
-  '';
-
-  scripts.unmount_firmware.exec = ''
-    set -exuo pipefail
-    (
-      cd "''${DEVENV_ROOT}"
-      umount src/
-    )
-  '';
-
-
-  #enterShell = ''
-  #'';
 
   # https://devenv.sh/languages/
   languages.nix.enable = true;
